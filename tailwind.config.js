63ba5a735a223b6c1a9163417df113ce093f5f72
yerlinson <yerlinson.lora@hytechhome.com>
2024-12-06 16:48:19 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+
 export default {
-  content: ["./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
 }
-
